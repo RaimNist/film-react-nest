dba3eb30c5f8a416f39da8faa84ea41cc7f68c8b
Vlad <raimnist@yandex.ru>
2026-03-28 00:30:34 +0300

    done

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,19 +1,30 @@
 import { Module } from '@nestjs/common';
-import {ServeStaticModule} from "@nestjs/serve-static";
-import {ConfigModule} from "@nestjs/config";
-import * as path from "node:path";
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as path from 'node:path';
 
-import {configProvider} from "./app.config.provider";
+import { configProvider } from './app.config.provider';
+import { FilmsModule } from './films/films.module';
+import { OrderModule } from './order/order.module';
+import { DatabaseConnection } from './repository/database.connection';
 
 @Module({
   imports: [
-	ConfigModule.forRoot({
-          isGlobal: true,
-          cache: true
-      }),
-      // @todo: Добавьте раздачу статических файлов из public
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+    }),
+    // @todo: Добавьте раздачу статических файлов из public
+    ServeStaticModule.forRoot({
+      rootPath: path.join(__dirname, '..', 'public', 'content', 'afisha'),
+      serveRoot: '/content/afisha',
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    FilmsModule,
+    OrderModule,
   ],
   controllers: [],
-  providers: [configProvider],
+  providers: [configProvider, DatabaseConnection],
 })
 export class AppModule {}
---
+++ backend/src/repository/database.connection.ts
@@ -0,0 +1,23 @@
+import { Injectable, OnModuleInit, Inject } from '@nestjs/common';
+import mongoose from 'mongoose';
+import { AppConfig } from '../app.config.provider';
+
+@Injectable()
+export class DatabaseConnection implements OnModuleInit {
+  constructor(@Inject('CONFIG') private readonly config: AppConfig) {}
+
+  async onModuleInit() {
+    if (this.config.database.driver === 'mongodb') {
+      try {
+        await mongoose.connect(this.config.database.url);
+        console.log('Подключено к MongoDB');
+      } catch (error) {
+        console.error('Ошибка подключения к MongoDB:', error);
+      }
+    } else {
+      console.error(
+        `Неподдерживаемый драйвер базы данных: ${this.config.database.driver}`,
+      );
+    }
+  }
+}
